b93b4638118d5202a6018bf6fa95d498897b09d8
Sneha Shukla <snehadefence090998@gmail.com>
2021-11-25 00:07:53 +0700

    End of Section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,34 +7,32 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
-
-
-
-
-
+import { Route, useLocation } from 'react-router';
+import HomePage from '../../features/activities/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-
-    activityStore.loadActivities();
-  }, [activityStore])
-
-  
-
-  if (activityStore.loadingInitial) return <LoadingComponent content= 'Loading app' />
+  const location = useLocation();
 
   return (
     <>
+      <Route exact path='/' component={HomePage} />
+      <Route
+         path={'/(.+)'}
+         render={() => (
+           <>
+             <NavBar  />
+             <Container style={{marginTop: '7em'}}>
+        
+               <Route exact path='/activities' component={ActivityDashboard} />
+               <Route path='/activities/:id' component={ActivityDetails} />
+               <Route key={location.key} path={['/createActivity','/manage/:id']} component={ActivityForm} />
+             </Container>
+           </>
+         )}
       
-      <NavBar  />
-       <Container style={{marginTop: '7em'}}>
-         
-          <ActivityDashboard  />
-        </Container>
-          
-      
+      />   
     </>
   );
 }
