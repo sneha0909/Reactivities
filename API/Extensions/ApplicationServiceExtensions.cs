a040c6d8b743018738127960e8e8e96d41fdbcd5
Sneha Shukla <snehadefence090998@gmail.com>
2022-03-18 08:00:31 +0530

    End of section 16

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,6 +10,7 @@ using Microsoft.OpenApi.Models;
 using Persistence;
 using Application.Interfaces;
 using Infrastructure.Security;
+using Infrastructure.Photos;
 
 namespace API.Extensions
 {
@@ -39,6 +40,8 @@ namespace API.Extensions
             services.AddMediatR(typeof(Application.Activities.List.Handler).Assembly);
             services.AddAutoMapper(typeof(Application.Core.MappingProfiles).Assembly);
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
             return services;
 
